d9d6c7fe8a009c103e28b8ac49fd1fc7c3802752
cathtessier <77817619+cathtessier@users.noreply.github.com>
2021-03-11 21:06:12 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,27 @@
 input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P1, Vd)
+    pins.digitalWritePin(DigitalPin.P2, Vg)
+    pins.digitalWritePin(DigitalPin.P12, 0)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+})
+input.onButtonPressed(Button.AB, function () {
+    pins.digitalWritePin(DigitalPin.P12, 0)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+})
+input.onButtonPressed(Button.B, function () {
     servos.P0.setAngle(100)
     basic.pause(500)
     servos.P0.setAngle(19)
 })
+let Vg = 0
+let Vd = 0
 servos.P0.setAngle(19)
+Vd = 1023
+Vg = 1023
 basic.forever(function () {
 	
 })
