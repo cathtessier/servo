f27a8210944b01f971d1de8c88bc94592c151c75
cathtessier <77817619+cathtessier@users.noreply.github.com>
2021-03-16 15:53:55 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,19 @@
 input.onButtonPressed(Button.A, function () {
-    pins.digitalWritePin(DigitalPin.P1, Vd)
-    pins.digitalWritePin(DigitalPin.P2, Vg)
-    pins.digitalWritePin(DigitalPin.P12, 0)
-    pins.digitalWritePin(DigitalPin.P13, 1)
-    pins.digitalWritePin(DigitalPin.P14, 1)
-    pins.digitalWritePin(DigitalPin.P15, 0)
+    Vd = 254
+    Vg = 254
+    for (let index = 0; index < 3; index++) {
+        pins.analogWritePin(AnalogPin.P1, Vg)
+        pins.analogWritePin(AnalogPin.P2, Vd)
+        pins.digitalWritePin(DigitalPin.P12, 1)
+        pins.digitalWritePin(DigitalPin.P13, 0)
+        pins.digitalWritePin(DigitalPin.P14, 0)
+        pins.digitalWritePin(DigitalPin.P15, 1)
+        Vg += 254
+        Vd += 254
+        basic.pause(100)
+    }
+    Vd = 1023
+    Vg = 1023
 })
 input.onButtonPressed(Button.AB, function () {
     pins.digitalWritePin(DigitalPin.P12, 0)
@@ -16,12 +25,16 @@ input.onButtonPressed(Button.B, function () {
     servos.P0.setAngle(100)
     basic.pause(500)
     servos.P0.setAngle(19)
+    basic.pause(500)
+    servos.P0.stop()
 })
 let Vg = 0
 let Vd = 0
 servos.P0.setAngle(19)
-Vd = 1023
-Vg = 1023
+basic.pause(1000)
+servos.P0.stop()
+Vd = 0
+Vg = 0
 basic.forever(function () {
 	
 })
